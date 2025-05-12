38a0fea688380eb9764e29be8fa4e5afc09716b1
YourNickname <kuromugi0715@gmail.com>
2025-05-12 11:58:45 +0900

    Initial Commit

---
+++ self-introduction/self_introductionApp.swift
@@ -0,0 +1,17 @@
+//
+//  self_introductionApp.swift
+//  self-introduction
+//
+//  Created by 黒本誉隆 on 2025/05/12.
+//
+
+import SwiftUI
+
+@main
+struct self_introductionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
